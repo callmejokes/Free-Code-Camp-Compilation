2fb41f65b9b82c971cff4e7825a55ffbeede81e8
ropelsive <157347453+callmejokes@users.noreply.github.com>
2026-08-28 17:23:20 +0800

    Insert winner and opponent IDs into games table

--- Build a World Cup Database/insert_data.sh
+++ Build a World Cup Database/insert_data.sh
@@ -28,6 +28,8 @@ do
       # insert team_id
       $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
     fi
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
